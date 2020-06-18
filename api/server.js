f3aae381ca1016828e7604ca29aeb74f86c436ca
Jordan <jordanshehane@gmail.com>
2020-06-17 18:44:01 -0600

    file set up, dependencies installed, migrations made

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const authenticate = require('../auth/restricted-middleware.js');
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
+const storiesRouter = require('../stories/stories-router.js');
+const picturesRouter = require('../pictures/pictures-router.js');
+
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use('/api/auth', authRouter);
+server.use('/api/users', authenticate, usersRouter);
+server.use('/api/stories', authenticate, storiesRouter);
+server.use('/api/pictures', authenticate, picturesRouter);
+
+module.exports = server;
