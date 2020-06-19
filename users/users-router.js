769a1994d5a338695a537ad99d198e560623221d
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-06-18 22:24:47 -0400

    updated router endpoints

--- users/users-router.js
+++ users/users-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const Users = require('./users-model.js');
+const { restart } = require('nodemon');
 
 router.get('/', (req, res) => {
     Users.find()
@@ -11,5 +12,20 @@ router.get('/', (req, res) => {
             res.send(error);
         })
 });
+
+router.get('/:id/stories', (req, res) => {
+    const { id } = req.params;
+    Users.findStories(id) 
+        .then(stories => {
+            if (stories.length) {
+                res.status(201).json(stories)
+            } else {
+                res.status(404).json({message: "Could not retrieve stories"})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({message: "Failed to get stories"})
+        })
+})
 
 module.exports = router;
